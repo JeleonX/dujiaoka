e1cfad26a795ee22e826bde04cc034441c0035d3
JeleonX <jeleonxu@gmail.com>
2025-01-17 13:59:49 +0800

    Update _footer.blade.php

--- resources/views/luna/layouts/_footer.blade.php
+++ resources/views/luna/layouts/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="layui-row">
         <div class="layui-col-md8 layui-col-md-offset2 layui-col-sm12">
             <div style="text-align: center">{!! dujiaoka_config_get('footer') !!}</div>
-            <p>Powered by @Dujiaoshuka</p>
+            <p>Powered by <a href="https://github.com/assimon/dujiaoka" target="_blank">@Dujiaoka</a></p>
         </div>
     </div>
 </div>
